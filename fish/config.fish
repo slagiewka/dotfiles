af9297cc8a79b19b4830a7a091a0edb6f290e0fa
Szymon Łągiewka <slagiewka@users.noreply.github.com>
2026-09-05 21:09:29 +0200

    go fish on macOS

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
+set -U fish_greeting
+
 if status is-interactive
-    mise activate fish | source
+    set --prepend PATH /opt/homebrew/bin /opt/homebrew/sbin
+    $HOME/.local/bin/mise activate fish | source
     starship init fish | source
     fzf --fish | source
     zoxide init fish | source
